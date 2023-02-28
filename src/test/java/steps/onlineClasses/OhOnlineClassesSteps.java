b9560a1cb4adfab23c7ba5e72b0ef21614ad1b34
Rodrigo <rockdrick999@gmail.com>
2023-02-28 13:01:14 -0300

    OHE online classes

--- src/test/java/steps/onlineClasses/OhOnlineClassesSteps.java
+++ src/test/java/steps/onlineClasses/OhOnlineClassesSteps.java
@@ -1,29 +1,16 @@
 package steps.onlineClasses;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
-import pages.BasePage;
-import pages.listing.listingPage;
 import pages.onlineClasses.OhOnlineClassesPage;
 
 public class OhOnlineClassesSteps {
     private final OhOnlineClassesPage _page;
     private final WebDriver _driver;
-    private final TestContext _context;
-
-    public OhOnlineClassesSteps(TestContext context) {
-        _context = context;
-        _driver = _context.driver;
-        _page = new OhOnlineClassesPage(_driver);
+    {
     }
 
+    public OhOnlineClassesSteps(OhOnlineClassesPage page, WebDriver driver) {
+        _page = page;
+        _driver = driver;
+    }
 }
